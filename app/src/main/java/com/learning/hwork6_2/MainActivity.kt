3846aded480e021d34fd84c7b9ff5ad22ab933b3
zahramarri <zahramarri.dev@gmail.com>
2022-06-01 18:51:59 +0430

    remove loop, add default text to view text, resize view text

--- app/src/main/java/com/learning/hwork6_2/MainActivity.kt
+++ app/src/main/java/com/learning/hwork6_2/MainActivity.kt
@@ -5,32 +5,30 @@ import android.os.Bundle
 import com.learning.hwork6_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    private var winner: Char? = null
+    private lateinit var binding: ActivityMainBinding
+    private var winner: Char = ' '
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        while (true) {
-            if (binding.editTextSquare1.text.isNotEmpty() ||
-                binding.editTextSquare2.text.isNotEmpty() ||
-                binding.editTextSquare3.text.isNotEmpty() ||
-                binding.editTextSquare4.text.isNotEmpty() ||
-                binding.editTextSquare5.text.isNotEmpty() ||
-                binding.editTextSquare6.text.isNotEmpty() ||
-                binding.editTextSquare7.text.isNotEmpty() ||
-                binding.editTextSquare8.text.isNotEmpty() ||
-                binding.editTextSquare9.text.isNotEmpty()
-            ) {
-                findWinner()
-            }
-            binding.textViewStatus.text = when (winner) {
+        if (binding.editTextSquare1.text.isNotEmpty() ||
+            binding.editTextSquare2.text.isNotEmpty() ||
+            binding.editTextSquare3.text.isNotEmpty() ||
+            binding.editTextSquare4.text.isNotEmpty() ||
+            binding.editTextSquare5.text.isNotEmpty() ||
+            binding.editTextSquare6.text.isNotEmpty() ||
+            binding.editTextSquare7.text.isNotEmpty() ||
+            binding.editTextSquare8.text.isNotEmpty() ||
+            binding.editTextSquare9.text.isNotEmpty()
+        ) {
+            findWinner()
+        }
+        binding.textViewStatus.text = when (winner) {
                 'O' -> "player O winner"
                 'X' -> "player X winner"
                 else -> "Draw"
             }
-        }
 
     }
 
@@ -40,25 +38,30 @@ class MainActivity : AppCompatActivity() {
             binding.editTextSquare1.text == binding.editTextSquare5.text &&
             binding.editTextSquare6.text == binding.editTextSquare5.text ||
             binding.editTextSquare1.text == binding.editTextSquare4.text &&
-            binding.editTextSquare7.text == binding.editTextSquare4.text) {
+            binding.editTextSquare7.text == binding.editTextSquare4.text
+        ) {
             winner = binding.editTextSquare1.text.first()
         }
         if (binding.editTextSquare2.text == binding.editTextSquare5.text &&
-            binding.editTextSquare8.text == binding.editTextSquare5.text) {
+            binding.editTextSquare8.text == binding.editTextSquare5.text
+        ) {
             winner = binding.editTextSquare2.text.first()
         }
         if (binding.editTextSquare3.text == binding.editTextSquare5.text &&
             binding.editTextSquare7.text == binding.editTextSquare5.text ||
             binding.editTextSquare3.text == binding.editTextSquare6.text &&
-            binding.editTextSquare9.text == binding.editTextSquare6.text) {
+            binding.editTextSquare9.text == binding.editTextSquare6.text
+        ) {
             winner = binding.editTextSquare3.text.first()
         }
         if (binding.editTextSquare4.text == binding.editTextSquare5.text &&
-            binding.editTextSquare6.text == binding.editTextSquare5.text) {
+            binding.editTextSquare6.text == binding.editTextSquare5.text
+        ) {
             winner = binding.editTextSquare4.text.first()
         }
         if (binding.editTextSquare7.text == binding.editTextSquare8.text &&
-            binding.editTextSquare9.text == binding.editTextSquare8.text) {
+            binding.editTextSquare9.text == binding.editTextSquare8.text
+        ) {
             winner = binding.editTextSquare7.text.first()
         }
     }
