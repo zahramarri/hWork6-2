0e9711528799c6f8174901f0c0c9d13826697e82
zahramarri <zahramarri.dev@gmail.com>
2022-06-01 16:13:13 +0430

    add view binding to the project and make ready to use it

--- app/src/main/java/com/learning/hwork6_2/MainActivity.kt
+++ app/src/main/java/com/learning/hwork6_2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.learning.hwork6_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learning.hwork6_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
