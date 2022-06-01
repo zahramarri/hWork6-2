205a7373524be953e409c00b61de4f54d85ffbb3
zahramarri <zahramarri.dev@gmail.com>
2022-06-01 16:25:41 +0430

    declare winner variable and set different statuses for textViewStatus by winner

--- app/src/main/java/com/learning/hwork6_2/MainActivity.kt
+++ app/src/main/java/com/learning/hwork6_2/MainActivity.kt
@@ -6,9 +6,20 @@ import com.learning.hwork6_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private var winner: Char? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textViewStatus.text = when (winner) {
+            'O' -> "player O winner"
+            'X' -> "player X winner"
+            else -> "Draw"
+        }
+
     }
+
+
+
 }
