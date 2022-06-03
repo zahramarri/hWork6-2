f8e1617c35e8b50703c98e798c0ab631c880ce4c
zahramarri <zahramarri.dev@gmail.com>
2022-06-03 18:23:56 +0430

    add more margins to squares, set textChangedListener, make 8 more functions to find winner and devote each to a square

--- app/src/main/java/com/learning/hwork6_2/MainActivity.kt
+++ app/src/main/java/com/learning/hwork6_2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.learning.hwork6_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.learning.hwork6_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,58 +14,252 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (binding.editTextSquare1.text.isNotEmpty() ||
-            binding.editTextSquare2.text.isNotEmpty() ||
-            binding.editTextSquare3.text.isNotEmpty() ||
-            binding.editTextSquare4.text.isNotEmpty() ||
-            binding.editTextSquare5.text.isNotEmpty() ||
-            binding.editTextSquare6.text.isNotEmpty() ||
-            binding.editTextSquare7.text.isNotEmpty() ||
-            binding.editTextSquare8.text.isNotEmpty() ||
-            binding.editTextSquare9.text.isNotEmpty()
-        ) {
-            findWinner()
-        }
-        binding.textViewStatus.text = when (winner) {
-                'O' -> "player O winner"
-                'X' -> "player X winner"
-                else -> "Draw"
+        binding.editTextSquare1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner1()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare2.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner2()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare3.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner3()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare4.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner4()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare5.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner5()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare6.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner6()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare7.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner7()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare8.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner8()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.editTextSquare9.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findWinner9()
+                binding.textViewStatus.text = when (winner) {
+                    'O' -> "player O winner"
+                    'X' -> "player X winner"
+                    else -> "Draw"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
 
     }
 
-    private fun findWinner() {
-        if (binding.editTextSquare1.text == binding.editTextSquare2.text &&
-            binding.editTextSquare3.text == binding.editTextSquare2.text ||
-            binding.editTextSquare1.text == binding.editTextSquare5.text &&
-            binding.editTextSquare6.text == binding.editTextSquare5.text ||
-            binding.editTextSquare1.text == binding.editTextSquare4.text &&
-            binding.editTextSquare7.text == binding.editTextSquare4.text
+    private fun findWinner1() {
+        if (binding.editTextSquare1.text.toString() == binding.editTextSquare2.text.toString() &&
+            binding.editTextSquare3.text.toString() == binding.editTextSquare2.text.toString() ||
+            binding.editTextSquare1.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare1.text.toString() == binding.editTextSquare4.text.toString() &&
+            binding.editTextSquare7.text.toString() == binding.editTextSquare4.text.toString()
         ) {
             winner = binding.editTextSquare1.text.first()
         }
-        if (binding.editTextSquare2.text == binding.editTextSquare5.text &&
-            binding.editTextSquare8.text == binding.editTextSquare5.text
+    }
+
+    private fun findWinner2() {
+        if (binding.editTextSquare1.text.toString() == binding.editTextSquare2.text.toString() &&
+            binding.editTextSquare3.text.toString() == binding.editTextSquare2.text.toString() ||
+            binding.editTextSquare2.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare8.text.toString() == binding.editTextSquare5.text.toString()
         ) {
             winner = binding.editTextSquare2.text.first()
         }
-        if (binding.editTextSquare3.text == binding.editTextSquare5.text &&
-            binding.editTextSquare7.text == binding.editTextSquare5.text ||
-            binding.editTextSquare3.text == binding.editTextSquare6.text &&
-            binding.editTextSquare9.text == binding.editTextSquare6.text
+    }
+
+    private fun findWinner3() {
+        if (binding.editTextSquare1.text.toString() == binding.editTextSquare2.text.toString() &&
+            binding.editTextSquare3.text.toString() == binding.editTextSquare2.text.toString() ||
+            binding.editTextSquare3.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare7.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare3.text.toString() == binding.editTextSquare6.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare6.text.toString()
         ) {
             winner = binding.editTextSquare3.text.first()
         }
-        if (binding.editTextSquare4.text == binding.editTextSquare5.text &&
-            binding.editTextSquare6.text == binding.editTextSquare5.text
+    }
+
+    private fun findWinner4() {
+        if (binding.editTextSquare4.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare6.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare1.text.toString() == binding.editTextSquare4.text.toString() &&
+            binding.editTextSquare7.text.toString() == binding.editTextSquare4.text.toString()
         ) {
             winner = binding.editTextSquare4.text.first()
         }
-        if (binding.editTextSquare7.text == binding.editTextSquare8.text &&
-            binding.editTextSquare9.text == binding.editTextSquare8.text
+    }
+
+    private fun findWinner5() {
+        if (binding.editTextSquare1.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare2.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare8.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare3.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare7.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare4.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare6.text.toString() == binding.editTextSquare5.text.toString()
+        ) {
+            winner = binding.editTextSquare5.text.first()
+        }
+    }
+
+    private fun findWinner6() {
+        if (binding.editTextSquare3.text.toString() == binding.editTextSquare6.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare6.text.toString() ||
+            binding.editTextSquare4.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare6.text.toString() == binding.editTextSquare5.text.toString()
+        ) {
+            winner = binding.editTextSquare6.text.first()
+        }
+    }
+
+    private fun findWinner7() {
+        if (binding.editTextSquare3.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare7.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare1.text.toString() == binding.editTextSquare4.text.toString() &&
+            binding.editTextSquare7.text.toString() == binding.editTextSquare4.text.toString() ||
+            binding.editTextSquare7.text.toString() == binding.editTextSquare8.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare8.text.toString()
         ) {
             winner = binding.editTextSquare7.text.first()
         }
     }
 
+    private fun findWinner8() {
+        if (binding.editTextSquare7.text.toString() == binding.editTextSquare8.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare8.text.toString() ||
+            binding.editTextSquare2.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare8.text.toString() == binding.editTextSquare5.text.toString()
+        ) {
+            winner = binding.editTextSquare8.text.first()
+        }
+    }
+
+    private fun findWinner9() {
+        if (binding.editTextSquare1.text.toString() == binding.editTextSquare5.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare5.text.toString() ||
+            binding.editTextSquare7.text.toString() == binding.editTextSquare8.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare8.text.toString() ||
+            binding.editTextSquare3.text.toString() == binding.editTextSquare6.text.toString() &&
+            binding.editTextSquare9.text.toString() == binding.editTextSquare6.text.toString()
+        ) {
+            winner = binding.editTextSquare9.text.first()
+        }
+    }
 }
